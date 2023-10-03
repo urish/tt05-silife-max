bc5212aae66725b3fa7841fe68d168c91618e119
Uri Shaked <uri@urishaked.com>
2023-10-03 13:48:37 +0300

    fix: tie unused grid inputs to zero
    also, move en/wr_en signals to pins ui_in[7:6], to allow larger grid sizes use additional ui_in pins for row/col select.
    
    bump utilization to 92% to accomodate the changes.

--- src/silife.v
+++ src/silife.v
@@ -14,8 +14,8 @@ module tt_um_urish_silife (
   assign uio_oe = 8'b00000000; 
   assign uio_out = 8'b00000000;
 
-  wire en = ui_in[3];
-  wire wr_en = ui_in[4];
+  wire en = ui_in[6];
+  wire wr_en = ui_in[7];
 
   grid_8x8 grid(
     .clk(clk),
@@ -24,7 +24,21 @@ module tt_um_urish_silife (
     .row_select(ui_in[2:0]),
     .clear_cells(wr_en ? ~uio_in : 8'b0),
     .set_cells(wr_en ? uio_in : 8'b0),
-    .cells(uo_out)
+    .cells(uo_out),
+    .i_n(0),
+    .i_e(0),
+    .i_s(0),
+    .i_w(0),
+    .i_ne(0),
+    .i_se(0),
+    .i_sw(0),
+    .i_nw(0),
+    .o_n(),
+    .o_w(),
+    .o_s(),
+    .o_e(),
+    .row_select2(0),
+    .cells2()
   );
 
 endmodule
