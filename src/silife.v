862dcf73ef2fc173d90e897dea8249e7d16b02b0
Uri Shaked <uri@urishaked.com>
2023-10-14 13:31:36 +0300

    feat: make game grid wrap around the edges

--- src/silife.v
+++ src/silife.v
@@ -1,5 +1,7 @@
 `default_nettype none
 
+`define GRID_WIDTH 8
+
 /* verilator lint_off PINCONNECTEMPTY */
 
 module tt_um_urish_silife (
@@ -19,6 +21,11 @@ module tt_um_urish_silife (
   wire en = ui_in[6];
   wire wr_en = ui_in[7];
 
+  wire [`GRID_WIDTH-1:0] grid_n;
+  wire [`GRID_WIDTH-1:0] grid_e;
+  wire [`GRID_WIDTH-1:0] grid_s;
+  wire [`GRID_WIDTH-1:0] grid_w;
+
   grid_8x8 grid(
     .clk(clk),
     .reset(!rst_n),
@@ -27,18 +34,18 @@ module tt_um_urish_silife (
     .clear_cells(wr_en ? ~uio_in : 8'b0),
     .set_cells(wr_en ? uio_in : 8'b0),
     .cells(uo_out),
-    .i_n(8'b0),
-    .i_e(8'b0),
-    .i_s(8'b0),
-    .i_w(8'b0),
-    .i_ne(1'b0),
-    .i_se(1'b0),
-    .i_sw(1'b0),
-    .i_nw(1'b0),
-    .o_n(),
-    .o_w(),
-    .o_s(),
-    .o_e(),
+    .i_n(grid_s),
+    .i_e(grid_w),
+    .i_s(grid_n),
+    .i_w(grid_e),
+    .i_ne(grid_s[0]),
+    .i_se(grid_w[0]),
+    .i_sw(grid_n[`GRID_WIDTH-1]),
+    .i_nw(grid_e[`GRID_WIDTH-1]),
+    .o_n(grid_n),
+    .o_e(grid_e),
+    .o_s(grid_s),
+    .o_w(grid_w),
     .row_select2(3'b0),
     .cells2()
   );
