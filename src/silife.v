bb3a7d8707ef6f6a4e4ee5414009ea4c00a9d97e
Uri Shaked <uri@urishaked.com>
2023-10-14 13:16:42 +0300

    refactor: switch to openlane 1
    also fix linter (verilator) warnings

--- src/silife.v
+++ src/silife.v
@@ -1,5 +1,7 @@
 `default_nettype none
 
+/* verilator lint_off PINCONNECTEMPTY */
+
 module tt_um_urish_silife (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
@@ -25,20 +27,27 @@ module tt_um_urish_silife (
     .clear_cells(wr_en ? ~uio_in : 8'b0),
     .set_cells(wr_en ? uio_in : 8'b0),
     .cells(uo_out),
-    .i_n(0),
-    .i_e(0),
-    .i_s(0),
-    .i_w(0),
-    .i_ne(0),
-    .i_se(0),
-    .i_sw(0),
-    .i_nw(0),
+    .i_n(8'b0),
+    .i_e(8'b0),
+    .i_s(8'b0),
+    .i_w(8'b0),
+    .i_ne(1'b0),
+    .i_se(1'b0),
+    .i_sw(1'b0),
+    .i_nw(1'b0),
     .o_n(),
     .o_w(),
     .o_s(),
     .o_e(),
-    .row_select2(0),
+    .row_select2(3'b0),
     .cells2()
   );
+
+  wire _unused_ok = &{
+    1'b0,
+    ena,
+    ui_in[5:3],
+    1'b0
+  };
 
 endmodule
